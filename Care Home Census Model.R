8858615b33f21c5eddd3c2764d46c73c13ed16bf
Benny24aa <harleyb101010@gmail.com>
2026-03-07 11:36:48 +0000

    Population Projections established, three groups - 65-74, 75-84, and 85 plus. Note: Couldn't get access to 95 + population size, so merged both together. This will be reflected in later analysis

--- Care Home Census Model.R
+++ Care Home Census Model.R
@@ -18,8 +18,24 @@ library(odbc)
 library(Metrics)
 library(jsonlite)
 library(phsopendata)
+library(tidyr)
+library(dplyr)
 
-Population_Projections <- get_resource(res_id = "7a9e74c9-8746-488b-8fba-0fad7c7866ea")
+
+Population_Projections <- get_resource(res_id = "7a9e74c9-8746-488b-8fba-0fad7c7866ea") %>% 
+  mutate(
+    `65-74`    = rowSums(select(., Age65:Age74), na.rm = TRUE),
+    `75-84`  = rowSums(select(., Age75:Age84), na.rm = TRUE),
+    `85 plus`  = rowSums(select(., Age85:Age90plus), na.rm = TRUE)
+  ) %>% 
+  select(-starts_with("Age")) %>% 
+  pivot_longer(
+    cols = c(`65-74`, `75-84`, `85 plus`), #### The limitation here is we can't break the data up in 95 +, so the age bracket will be 85 + and aggregate others row together to account for this
+    names_to = "Age",
+    values_to = "Population"
+  ) %>% 
+  select(Year, Sex, Age, Population)
+
 
 Care_Home_Census_Health_Characteristics <- get_resource(res_id = "92ebf3df-2af4-4d73-9397-f5d6a6778da7")
 
