7c6114a2c99e0a0a3a0fc7e376e46e51bb0c84b8
Benny24aa <harleyb101010@gmail.com>
2026-03-12 08:23:40 +0000

    validation plots for report

--- Care Home Census Model.R
+++ Care Home Census Model.R
@@ -34,7 +34,8 @@ Population_Projections <- get_resource(res_id = "7a9e74c9-8746-488b-8fba-0fad7c7
     names_to = "Age",
     values_to = "Population"
   ) %>% 
-  select(Year, Sex, Age, Population) 
+  select(Year, Sex, Age, Population) %>% 
+  filter(Year < "2036")
 
 
 
@@ -82,8 +83,12 @@ Utilisation <- Utilisation %>%
   ungroup()
 
 LatestRates <- Utilisation %>%
-  filter(Year == "2022")%>%
-  select(Age, Sex, UtilisationRate)
+  filter(Year >= 2020) %>%
+  group_by(Age, Sex) %>%
+  summarise(
+    UtilisationRate = mean(UtilisationRate, na.rm = TRUE),
+    .groups = "drop"
+  )
 
 Forecast <- Population_Projections %>%
   left_join(LatestRates, by = c("Age", "Sex")) %>%
@@ -118,7 +123,8 @@ p2 <- Forecast %>%
     y = ~ProjectedResidents,
     color = ~Sex,
     type = "scatter",
-    mode = "lines+markers"
+    mode = "lines+markers",
+    showlegend = FALSE
   ) %>%
   layout(
     title = "Projected Care Home Residents (Age 75–84)",
@@ -135,15 +141,59 @@ p3 <- Forecast %>%
     y = ~ProjectedResidents,
     color = ~Sex,
     type = "scatter",
-    mode = "lines+markers"
+    mode = "lines+markers",
+    showlegend = FALSE
   ) %>%
   layout(
-    title = "Projected Care Home Residents (Age 85+)",
+    title = "Projected Care Home Residents Long Term (Age 65 +)",
     xaxis = list(title = "Year"),
     yaxis = list(title = "Projected Residents")
   )
 
 p3
+
+Forecast_Aggregated <- Forecast %>%
+  filter(Age %in% c("65-74", "75-84", "85 plus")) %>%
+  group_by(Year, Sex) %>%
+  summarise(ProjectedResidents = sum(ProjectedResidents, na.rm = TRUE),
+            .groups = "drop")
+
+p_total_sex <- Forecast_Aggregated %>%
+  plot_ly(
+    x = ~Year,
+    y = ~ProjectedResidents,
+    color = ~Sex,
+    type = "scatter",
+    mode = "lines+markers"
+  ) %>%
+  layout(
+    title = "Projected Care Home Residents (Age 65+)",
+    xaxis = list(title = "Year"),
+    yaxis = list(title = "Projected Residents")
+  )
+
+p_total_sex
+
+library(plotly)
+
+combined_plot <- plotly::subplot(
+  p1, p2, p3,
+  nrows = 1,
+  shareY = TRUE
+) %>%
+  layout(
+    margin = list(t = 80),   # increase top space
+    annotations = list(
+      list(text = "Age 65–74", x = 0.065, y = 1.05, xref = "paper", yref = "paper",
+           showarrow = FALSE, font = list(size = 14)),
+      list(text = "Age 75–84", x = 0.50, y = 1.05, xref = "paper", yref = "paper",
+           showarrow = FALSE, font = list(size = 14)),
+      list(text = "Age 85+", x = 0.90, y = 1.05, xref = "paper", yref = "paper",
+           showarrow = FALSE, font = list(size = 14))
+    )
+  )
+
+combined_plot
 
 
 actuals <- Residents %>% 
@@ -191,7 +241,7 @@ Forecast_Plot <- Forecast_Validate %>%
 
 library(plotly)
 
-plot_ly(
+validation_graph <- plot_ly(
   Forecast_Plot,
   x = ~Year,
   y = ~Value,
@@ -220,5 +270,92 @@ Registered_Places <- get_resource(res_id = "04958b74-a351-4dc0-b8e4-cbc369372804
   rename(RegisteredPlaces = Value)
 
 CurrentPlaces <- Registered_Places %>%
-  filter(Year == 2022) %>%
+  filter(Year == 2025) %>%
   pull(RegisteredPlaces)
+
+
+assumed_occupancy <- 0.86 #### PHS reported this as an estimate in 2025
+
+Forecast_All <- Forecast_All %>%
+  mutate(
+    RequiredPlaces = ProjectedResidents / assumed_occupancy,
+    CurrentCapacity = CurrentPlaces,
+    CapacityGap = RequiredPlaces - CurrentCapacity
+  )
+
+Forecast_All  <- Forecast_All  %>%
+  mutate(
+    HighDemand = RequiredPlaces * 1.05,
+    LowDemand  = RequiredPlaces * 0.95
+  )
+
+plot_ly(Forecast_All) %>%
+  add_lines(x = ~Year, y = ~RequiredPlaces, name = "Baseline Demand") %>%
+  add_lines(x = ~Year, y = ~HighDemand, name = "High Demand Scenario") %>%
+  add_lines(x = ~Year, y = ~LowDemand, name = "Low Demand Scenario") %>%
+  add_lines(x = ~Year, y = ~CurrentCapacity, name = "Current Capacity") %>%
+  layout(
+    title = "Projected Care Home Capacity Requirement (Age 65+)",
+    xaxis = list(title = "Year"),
+    yaxis = list(title = "Places Required")
+  )
+
+
+Actuals_Sex <- Residents %>%
+  group_by(Year, Sex) %>%
+  summarise(
+    Residents = sum(Residents, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+Forecast_Sex <- Forecast %>%
+  group_by(Year, Sex) %>%
+  summarise(
+    ProjectedResidents = sum(ProjectedResidents, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+Validation_Sex <- Actuals_Sex %>%
+  full_join(Forecast_Sex, by = c("Year","Sex")) %>% 
+  filter(Year > 2021)
+
+Validation_Plot_Sex <- Validation_Sex %>%
+  pivot_longer(
+    cols = c(Residents, ProjectedResidents),
+    names_to = "Series",
+    values_to = "Value"
+  ) %>%
+  mutate(
+    Series = recode(
+      Series,
+      Residents = "Actual",
+      ProjectedResidents = "Forecast"
+    ),
+    LineGroup = paste(Sex, Series)
+  )
+
+line_colors <- c(
+  "Male Actual" = "#1f77b4",
+  "Female Actual" = "#e377c2",
+  "Male Forecast" = "#ff7f0e",
+  "Female Forecast" = "#17becf"
+)#17becf
+
+validation_graph_sex <- plot_ly(
+  Validation_Plot_Sex,
+  x = ~Year,
+  y = ~Value,
+  color = ~LineGroup,
+  colors = line_colors,
+  type = "scatter",
+  mode = "lines+markers"
+) %>%
+  layout(
+    title = "Actual vs Forecast Care Home Residents by Sex (2022 onwards)",
+    xaxis = list(title = "Year"),
+    yaxis = list(title = "Residents"),
+    legend = list(title = list(text = "Series"))
+  )
+
+validation_graph_sex
+
